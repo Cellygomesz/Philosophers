bf929be8458834521729cab466f081a459b0af79
mgomes-s <marcellygomes003@gmail.com>
2025-03-13 13:47:15 -0300

    mexendo na atoi

--- aux.c
+++ aux.c
@@ -6,7 +6,41 @@
 /*   By: mgomes-s <mgomes-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 15:00:43 by mgomes-s          #+#    #+#             */
-/*   Updated: 2025/03/12 15:00:43 by mgomes-s         ###   ########.fr       */
+/*   Updated: 2025/03/13 13:46:42 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	neg;
+	int	temp;
+
+	i = 0;
+	neg = 1;
+	temp = 0;
+	while (nptr[i])
+	{
+		// fazer a verificação de sinais ainda @#$% 
+		if (nptr[i] >= 'a' && nptr[i] <= 'z')
+			return (error(2));
+		if (nptr[i] >= 'A' && nptr[i] <= 'Z')
+			return (error(2));
+		i++;
+	}
+	i = 0;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+	{
+		if (nptr[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		temp = (nptr[i] - '0') + (temp * 10);
+		i++;
+	}
+	return (temp * neg);
+}
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mgomes-s <mgomes-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 14:38:23 by mgomes-s          #+#    #+#             */
-/*   Updated: 2025/03/12 15:37:23 by mgomes-s         ###   ########.fr       */
+/*   Updated: 2025/03/13 11:08:51 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,18 @@
 
 int	main(int ac, char **av)
 {
+	t_philo	*p;
 
-	if (!av[1])
+	if (!(av[1] || av[2] || av[3] || av[4] || av[5]))
+	{
+		write(2, "Error\n", 6);
 		exit(0);
-	else if 
+	}
+	p->philosophers = ft_atoi(av[1]);
+	p->time_die = ft_atoi(av[2]);
+	p->time_eat = ft_atoi(av[3]);
+	p->time_sleep = ft_atoi(av[4]);
+	p->philo_must_eat = ft_atoi(av[5]);
+
 	return (0);
 }
