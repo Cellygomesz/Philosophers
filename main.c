7c029bc933f7450281dd5c7d9977525c0f5ebe7d
mgomes-s <marcellygomes003@gmail.com>
2025-03-21 02:44:36 -0300

    madrugada

--- main.c
+++ main.c
@@ -6,12 +6,35 @@
 /*   By: mgomes-s <mgomes-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 14:38:23 by mgomes-s          #+#    #+#             */
-/*   Updated: 2025/03/13 16:04:13 by mgomes-s         ###   ########.fr       */
+/*   Updated: 2025/03/21 02:43:12 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int main(int ac, char **av)
+{
+    t_philo info;
+    t_philosopher *philosophers;
+    pthread_mutex_t *forks;
+
+    // Preenche info com argumentos do programa (exemplo simplificado)
+    info.philosophers = 5;
+    info.time_die = 800;
+    info.time_eat = 200;
+    info.time_sleep = 200;
+    info.philo_must_eat = -1; // Opcional
+
+    // Inicializa mutexes e filósofos dinamicamente
+    forks = init_forks(info.philosophers);
+    philosophers = init_philosophers(info.philosophers, forks);
+
+    // Criar as threads depois...
+
+    return (0);
+}
+
+/*
 int	main(int ac, char **av)
 {
 	t_philo	*p;
@@ -28,14 +51,13 @@ int	main(int ac, char **av)
 	p->time_sleep = is_num(av[4]);
 	p->philo_must_eat = is_num(av[5]);
 
-	/*
 	printf("1: %d\n", p->philosophers);
 	printf("2: %d\n", p->time_die);
 	printf("3: %d\n", p->time_eat);
 	printf("4: %d\n", p->time_sleep);
 	printf("5: %d\n", p->philo_must_eat);
-*/
-
 	free(p);
 	return (0);
 }
+*/
+
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: mgomes-s <mgomes-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 15:22:00 by mgomes-s          #+#    #+#             */
-/*   Updated: 2025/03/13 15:29:51 by mgomes-s         ###   ########.fr       */
+/*   Updated: 2025/03/21 02:41:35 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,16 @@ typedef struct s_philo
 	int	philo_must_eat;
 
 } t_philo;
+
+typedef struct s_philosopher
+{
+    int id;                      // ID do filósofo (0 a N-1)
+    int meals_eaten;             // Quantidade de vezes que ele já comeu
+    pthread_t thread;            // Thread associada ao filósofo
+    pthread_mutex_t *left_fork;  // Ponteiro para o garfo à esquerda
+    pthread_mutex_t *right_fork; // Ponteiro para o garfo à direita
+} t_philosopher;
+
 
 int	ft_atoi(char *nptr);
 int	error(int fd);
