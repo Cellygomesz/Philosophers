4d2a1e25200ad364833f63f6f83bd782e1064b77
mgomes-s <marcellygomes003@gmail.com>
2025-03-31 15:57:15 -0300

    eu realmente preciso terminar isso

--- main.c
+++ main.c
@@ -6,54 +6,29 @@
 /*   By: mgomes-s <mgomes-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 14:38:23 by mgomes-s          #+#    #+#             */
-/*   Updated: 2025/03/26 15:16:08 by mgomes-s         ###   ########.fr       */
+/*   Updated: 2025/03/31 15:56:10 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	init_num(t_philo *p,char **av)
+void	init_num(t_rules *p,char **av)
 {
-	p->philosophers = valid_inputs(av[1]);
-	p->time_die = valid_inputs(av[2]);
-	p->time_eat = valid_inputs(av[3]);
-	p->time_sleep = valid_inputs(av[4]);
-	if (av[5])
-		p->philo_must_eat = valid_inputs(av[5]);
-}
+	p->num_philos = valid_inputs(av[1]);
+	p->time_to_die = valid_inputs(av[2]);
+	p->time_to_eat = valid_inputs(av[3]);
+	p->time_to_sleep = valid_inputs(av[4]);
 
-void	init_mutex(t_philo *p)
-{
-	int	i;
-
-	i = 0;
-	p->forks = malloc(sizeof(pthread_mutex_t) * p->philosophers);
-		if (!p->forks)
-			return (error(2));
-	while (i < p->philosophers)
-	{
-		pthread_mutex_init(&p->forks[i], NULL);
-		i++;
-	}
 }
 
 int	main(int ac, char **av)
 {
-	t_philo	*p;
+	t_rules	*p;
 
 	p = malloc(sizeof(t_philo));
 	if (ac == 5 || ac == 6)
 	{
 		init_num(p, av);
-		init_mutex(p);
-
-		while (i < p->philosophers)
-		{
-			pthread_mutex_destroy(&p->forks[i]);
-			i++;
-		}
-		free(p->forks);
-
 	}
 	else
 	{
@@ -61,11 +36,11 @@ int	main(int ac, char **av)
 		free(p);
 		exit(0);
 	}
-	/* printf("1: %d\n", p->philosophers);
+	printf("1: %d\n", p->philosophers);
 	printf("2: %d\n", p->time_die);
 	printf("3: %d\n", p->time_eat);
 	printf("4: %d\n", p->time_sleep);
-	printf("5: %d\n", p->philo_must_eat); */
+	printf("5: %d\n", p->philo_must_eat);
 	return (0);
 }
 
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: mgomes-s <mgomes-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 15:22:00 by mgomes-s          #+#    #+#             */
-/*   Updated: 2025/03/26 14:21:17 by mgomes-s         ###   ########.fr       */
+/*   Updated: 2025/03/31 15:42:46 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,25 @@
 # include <stdlib.h>
 # include <pthread.h>
 
-typedef struct s_philo
-{
-	int	philosophers;
-	int	time_die;
-	int	time_eat;
-	int	time_sleep;
-	int	philo_must_eat;
-	pthread_mutex_t	forks;
-	t_philosopher	*philosophers;
+typedef struct s_rules {
+    int	num_philos;         // Número total de filósofos
+    int	time_to_die;        // Tempo máximo que um filósofo pode ficar sem comer antes de morrer (ms)
+    int	time_to_eat;        // Tempo que um filósofo leva para comer (ms)
+    int	time_to_sleep;      // Tempo que um filósofo leva para dormir (ms)
+    pthread_mutex_t	*forks; // Array de mutexes representando os garfos
+    pthread_mutex_t	write_mutex; // Mutex para evitar condições de corrida na impressão
+    int	someone_died;       // Flag para indicar se um filósofo morreu (1 = morreu, 0 = continua)
+} t_rules;
 
+typedef struct s_philo {
+    int	id;                // ID do filósofo (vai de 0 a num_philos - 1)
+    long	last_meal_time;   // Timestamp da última refeição do filósofo (usado para monitorar a fome)
+    int	meals_eaten;       // Contador de quantas vezes o filósofo já comeu
+    pthread_mutex_t	*left_fork;  // Ponteiro para o mutex do garfo à esquerda
+    pthread_mutex_t	*right_fork; // Ponteiro para o mutex do garfo à direita
+    pthread_mutex_t	meal_mutex;  // Mutex para proteger o acesso ao tempo da última refeição
+    t_rules	*rules;        // Ponteiro para acessar as regras e configurações globais do programa
 } t_philo;
-
-typedef struct s_philosopher
-{
-    int id;                      // ID do filósofo (0 a N-1)
-    int meals_eaten;             // Quantidade de vezes que ele já comeu
-    pthread_t thread;            // Thread associada ao filósofo
-    pthread_mutex_t *left_fork;  // Ponteiro para o garfo à esquerda
-    pthread_mutex_t *right_fork; // Ponteiro para o garfo à direita
-} t_philosopher;
-
 
 int	ft_atoi(char *nptr);
 int	error(int fd);
